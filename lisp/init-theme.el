25b6988a4a5dbd8895845e341a2891712acdaa7f
Jason Divock <jdivock@gmail.com>
2015-02-12 16:41:43 -0500

    woo, it's working, and got theming to ignore prompts

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,6 +1,11 @@
 (require-package 'color-theme-sanityinc-tomorrow)
+(require-package 'color-theme-sanityinc-solarized)
 
-(load-theme 'sanityinc-tomorrow-night)
+;;------------------------------------------------------------------------------
+;; New-style theme support, in which per-frame theming is not possible
+;;------------------------------------------------------------------------------
+
+(load-theme 'sanityinc-tomorrow-night t)
 
 ;(load-theme 'sanityinc-tomorrow-day)
 (provide 'init-theme)
