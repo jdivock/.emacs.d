31e7eb8f8045d3963097a255e99a073c4ecf9c2d
Jason Divock <jdivock@box.com>
2015-03-12 13:29:49 -0700

    added php smarty stuff

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -5,6 +5,7 @@
 ;;(require 'web-mode)
 ;;(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(add-to-list 'auto-mode-alist '("\\.tmpl$" . html-mode))
 (add-hook 'html-mode-hook 'ac-html-enable)
 ;; (add-to-list 'web-mode-ac-sources-alist
 ;;              '("html" . (ac-source-html-attribute-value
