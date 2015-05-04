db261dbbc67f2c93ea96b3f561f64a380aec20f3
Jason Divock <jdivock@box.com>
2015-05-04 09:54:04 -0700

    Finally moved jabber credentials to be private

---
+++ lisp/init-diminish.el
@@ -0,0 +1,11 @@
+(require-package 'diminish)
+
+(require 'magit)
+(diminish 'projectile-mode)
+(diminish 'yas-minor-mode)
+(diminish 'auto-complete-mode)
+(diminish 'magit-auto-revert-mode)
+(diminish 'undo-tree-mode)
+;(diminish 'auto-fill-mode)
+
+(provide 'init-diminish)
